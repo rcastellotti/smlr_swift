8f142e068f0f368092bb2e5ac3584f758ee29790
Roberto Castellotti <me@rcastellotti.dev>
2025-03-20 23:29:13 +0100

    Initial Commit

---
+++ smlr_swift/smlr_swiftApp.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {Text("smlr_swift") }.padding()
+    }
+}
+
+@main
+struct smlr_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
